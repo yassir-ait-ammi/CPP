cf82573871deb59010a2231709fae42114107e66
yassir-ait-ammi <yassiraitammi@gamil.com>
2025-09-15 18:36:23 +0100

    CPP Module 02 DONE::

--- CPP_module02/ex03/main.cpp
+++ CPP_module02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 10:26:22 by yaait-am          #+#    #+#             */
-/*   Updated: 2025/06/25 15:01:22 by yaait-am         ###   ########.fr       */
+/*   Updated: 2025/09/15 18:36:07 by yaait-am         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 int main()
 {
 	// insisde :
-	//Point a(10, 10);
-	//Point b(10, 10);
-	//Point c(10, 10);
-	//Point p(0, 0);
+	Point a(10, 10);
+	Point b(10, 10);
+	Point c(10, 10);
+	Point p(0, 0);
 	// outside :
-	Point a(0, 0);
-	Point b(10, 0);
-	Point c(5, 10);
-	Point p(20, 5);
+	//Point a(0, 0);
+	//Point b(10, 0);
+	//Point c(5, 10);
+	//Point p(20, 5);
 	if (bsp(a, b, c, p))
 		std::cout << "The point is inside the triangle." << std::endl;
 	else
